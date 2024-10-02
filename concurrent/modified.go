a0269a6a34ea177eb817799e10978c6c3e44c907
cg-manish <manish@macgain.net>
2024-10-01 20:02:34 -0500

    init go repostiory and initial implmentation of goroutines for concurrent site traversal

---
+++ concurrent/modified.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+	"time"
+)
+
+const (
+	maxConcurrentRequests = 10  // Limit of concurrent requests
+	totalRequests         = 100 // Total number of requests
+)
+
+var http_client *http.Client
+
+func init() {
+	transport := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	http_client = &http.Client{Transport: transport, Timeout: 2 * time.Second}
+}
+
+func convertIpToFileName(ip string) string {
+	return strings.Replace(ip, ":", "-", -1)
+}
+
+func convertFileNameToIP(filename string) string {
+	return strings.Replace(filename, "-", ":", -1)
+}
+
+func sendRequest(ip string, port int, wg *sync.WaitGroup, channelSemaphore chan struct{}) {
+	defer wg.Done()
+	defer func() { <-channelSemaphore }()
+
+	protocol := "http"
+	if port == 443 {
+		protocol = "https"
+	}
+	url := fmt.Sprintf("%s://[%s]:%d", protocol, ip, port)
+	fmt.Println(url)
+	resp, err := http_client.Get(url)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	status_code := resp.StatusCode
+
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Chek if dir exists for that status code.
+	_, err = os.Stat(fmt.Sprintf("response/%d", status_code))
+	if err != nil {
+		os.Mkdir(fmt.Sprintf("response/%d", status_code), os.ModePerm)
+	}
+
+	os.WriteFile(fmt.Sprintf("response/%d/%s.html", status_code, convertIpToFileName(ip)), body, os.ModePerm)
+
+	// write to csv file with ip ,status and port
+	csv_row := fmt.Sprintf("%s,%d,%d\n", ip, port, status_code)
+
+	// append to file
+	os.WriteFile("output.csv", []byte(csv_row), os.ModeAppend)
+
+}
+
+func main() {
+
+	var wg sync.WaitGroup
+	channelSemaphore := make(chan struct{}, maxConcurrentRequests)
+
+	buff, err := os.ReadFile("./80.txt")
+	if err != nil {
+		fmt.Println("Error", err)
+		os.Exit(1)
+	}
+
+	ips := strings.Split(string(buff), "\n")
+
+	for _, ip := range ips {
+		if ip == "" {
+			continue
+		}
+
+		channelSemaphore <- struct{}{} // add a semaphore to the channel
+		wg.Add(1)
+
+		go sendRequest(ip, 80, &wg, channelSemaphore)
+	}
+	wg.Wait() // Wait for all goroutines to finish
+}
